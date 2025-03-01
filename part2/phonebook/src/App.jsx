4ed904e472337c284bcc550fbfc3625e217b4e4c
Riku Heino <heino.riku@gmail.com>
2025-03-01 14:31:51 +0200

    Fixed this mess, now it works as implemented

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,40 +1,76 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567", id: 1 },
+    { name: "Seppo Taalasmaa", number: "040-654651", id: 2 },
+    { name: "Ismo Laitela", number: "040-9874016", id: 3 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setFilter] = useState("");
 
-  // Handle input change
+  // Handle input changes
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  // Searching & filtering function
+  function filterItems(array, query) {
+    return array.filter((element) =>
+      element.name.toLowerCase().includes(query.toLowerCase())
+    );
+  }
 
   // Handle form submission
   const addPerson = (event) => {
     event.preventDefault(); // Prevent page reload
-    const newPerson = { name: newName };
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1, // check length of array and set ID for new entry
+    };
+
+    // Check if person exists and throw error if so
+    if (persons.some((person) => person.name === newPerson.name)) {
+      alert(`${newName} is already added to the phonebook`);
+      return;
+    }
+
     setPersons(persons.concat(newPerson)); // Add new person to the list
     setNewName(""); // Clear input field
+    setNewNumber("");
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      filter: <input value={newFilter} onChange={handleFilterChange} />
+      <h2>Add New</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
-        ))}
-      </ul>
-      <div>debug: {newName}</div>
+      {/* Use filtering function to display filtered person*/}
+      {filterItems(persons, newFilter).map((person) => (
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
+      ))}
     </div>
   );
 };
